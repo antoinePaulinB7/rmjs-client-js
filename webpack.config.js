37197cb723fa8b0ed69a3c601fa48f32b200c74c
antoinePaulinB7 <apaulinb7@gmail.com>
2021-04-23 22:05:12 -0400

    adding index.html to webpack + start script

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -20,4 +21,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 };
